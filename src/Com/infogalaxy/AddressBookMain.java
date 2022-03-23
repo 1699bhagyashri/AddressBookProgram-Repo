fd5e6fd18878949f42032264fb98c4d967b713b2
1699bhagyashri <bhagipatil1999@gmail.com>
2022-03-23 13:23:06 +0530

    MODIFIED: Edit contact

--- src/Com/infogalaxy/AddressBookMain.java
+++ src/Com/infogalaxy/AddressBookMain.java
@@ -26,10 +26,35 @@ public class AddressBookMain {
           private void showContact() {
               System.out.println(contact.toString());
           }
-            public static void main(String[] args) {
+
+          public void editContact() {
+              System.out.println("Enter the First name");
+              String Firstname = scanner.next();
+              if (Firstname.equals(contact.getFirstName())) {
+                  System.out.println("Contact is fount");
+                  System.out.println("Enter last name");
+                  contact.setLastName(scanner.next());
+                  System.out.println("Enter city name");
+                  contact.setCity(scanner.next());
+                  System.out.println("Enter the state name");
+                  contact.setState(scanner.next());
+                  System.out.println("Enter zip code");
+                  contact.setZip(scanner.next());
+                  System.out.println("Enter the email Id");
+                  contact.setEmail(scanner.next());
+                  System.out.println("Enter mobile number");
+                  contact.setPhoneNumber(scanner.next());
+              } else {
+                  System.out.println("Contact not fount");
+              }
+          }
+
+              public static void main (String[] args) {
         System.out.println("Welcome to Address book program");
         AddressBookMain addressBookMain = new AddressBookMain();
         addressBookMain.addContact();
+        addressBookMain.showContact();
+        addressBookMain.editContact();
         addressBookMain.showContact();
     }
 
